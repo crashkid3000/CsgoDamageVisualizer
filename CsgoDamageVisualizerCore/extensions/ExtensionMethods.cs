ca3c317558d5c7f5f3df3566664a8de665e8ece3
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-01 22:53:55 +0200

    Added basic frame for state-driven CfgLoader

---
+++ CsgoDamageVisualizerCore/extensions/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerCore.extensions
+{
+    public static class ExtensionMethods
+    {
+        public static int GetIndentaionLevel(this string s)
+        {
+            bool firstAlphaCharSeen = false;
+            return s.Count(c =>
+            {
+                if (Char.IsLetter(c))
+                {
+                    firstAlphaCharSeen = true;
+                }
+                return c.Equals(' ') && !firstAlphaCharSeen;
+            });
+        }
+    }
+}
--- CsgoDamageVisualizerCore/loader/CfgLoader.cs
+++ CsgoDamageVisualizerCore/loader/CfgLoader.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+
+using CsgoDamageVisualizerCore.loader.model;
+using CsgoDamageVisualizerCore.extensions;
 
 namespace CsgoDamageVisualizerCore.loader
 {
     public class CfgLoader
     {
+
+        private const string WEAPON_PREFAB_START_PATTERN = @"\s+weapon_\w+_prefab\s*";
+        private const string OPENING_BRACES_PATTERN = @"\s+\{\s+";
+        private const string CLOSING_BRACES_PATTERN = @"\s+\}\s+";
+
+        private State currentState = State.BEFORE_FIRST;
+        private int currentIndentationLevel = 0;
+        private int weaponDefintitionIdentationLevel = 0;
 
         public CfgLoader()
         {
@@ -15,6 +29,23 @@ namespace CsgoDamageVisualizerCore.loader
         }
 
         private ICsgoDamageVisualizerConfig iCsgoDamageVisualizerConfig;
+
+        public IReadOnlyDictionary<string, CfgWeapon> ParseCfgFile()
+        {
+            Dictionary<string, CfgWeapon> weapons = new Dictionary<string, CfgWeapon>();
+
+            string[] lines = LoadCfgFile();
+
+            foreach(string line in lines)
+            {
+                findNewState(line);
+
+                switch (currentState)
+                {
+
+                }
+            }
+        }
 
         //Find CFG file
 
@@ -41,11 +72,71 @@ namespace CsgoDamageVisualizerCore.loader
             return readLines.Result;
         }
 
+        /// <summary>
+        /// Gets the location of the items_game.txt from the CSGO install dir
+        /// </summary>
+        /// <returns></returns>
         private Uri? GetConfigFileLocation()
         {
             Uri installDir = iCsgoDamageVisualizerConfig.GetCsgoInstallDir();
             Uri retVal = new Uri(installDir, @"csgo\scripts\items\items_game.txt");
             return retVal;
         }
+
+        private void findNewState(string line)
+        {
+            if (Regex.IsMatch(line, OPENING_BRACES_PATTERN) {
+                currentIndentationLevel++;
+            }
+            
+            if(Regex.IsMatch(line, CLOSING_BRACES_PATTERN))
+            {
+                currentIndentationLevel--;
+            }
+
+            switch (currentState)
+            {
+                case State.BEFORE_FIRST:
+                    {
+                        if (Regex.IsMatch(line, WEAPON_PREFAB_START_PATTERN))
+                        {
+                            currentState = State.IN_WEAPON_DEFINITION;
+                            weaponDefintitionIdentationLevel = currentIndentationLevel;
+                        }
+                        break;
+                    }
+                case State.BETWEEN_WEAPON_DEFINITION:
+                    {
+                        if (Regex.IsMatch(line, WEAPON_PREFAB_START_PATTERN))
+                        {
+                            currentState = State.IN_WEAPON_DEFINITION;
+                        }
+                        break;
+                    }
+                case State.IN_WEAPON_DEFINITION:
+                    {
+                        if (Regex.IsMatch(line, CLOSING_BRACES_PATTERN) && currentIndentationLevel == weaponDefintitionIdentationLevel)
+                        {
+
+                            currentState = State.BETWEEN_WEAPON_DEFINITION;
+                        }
+                        break;
+                    }
+            }
+        }
+
+      
+
+
+        private enum State
+        {
+            BEFORE_FIRST,
+            IN_WEAPON_DEFINITION,
+            BETWEEN_WEAPON_DEFINITION;
+        }
+
+
     }
+
+    
 }
