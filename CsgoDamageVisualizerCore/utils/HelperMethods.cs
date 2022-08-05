052b458caab504938d1aa508ae8ceade7d9c6bd0
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-05 17:52:47 +0200

    Added helper methods, renamed dir for helper methods

---
+++ CsgoDamageVisualizerCore/utils/HelperMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerCore.utils
+{
+    public class HelperMethods
+    {
+
+        public enum Project
+        {
+            CORE,
+            DESKTOP,
+            ASPX,
+            TEST
+        }
+
+        private static Dictionary<Project, string> projectDirBaseNames = new Dictionary<Project, string>()
+        {
+            [Project.CORE] = "CsgoDamageVisualizerCore",
+            [Project.DESKTOP] = "CSgtoDamageVisualizer",
+            [Project.ASPX] = "CsgoDamageVisualizerWeb",
+            [Project.TEST] = "CsgoDamageVisualizerTests"
+        };
+
+        public Uri GetProjectBaseDirt(Project project, [CallerFilePath] string sourceFilePath = "a#b")
+        {
+
+            if (sourceFilePath.Equals("a#b"))
+            {
+                throw new Exception("Could not get CallerFilePath");
+            }
+            DirectoryInfo dirInfo = new DirectoryInfo(sourceFilePath);
+            while (dirInfo != null && !dirInfo.Name.Equals("CsgoDamageVisualizer"))
+            {
+                dirInfo = dirInfo.Parent;
+            }
+            return new Uri(new Uri(dirInfo.FullName), projectDirBaseNames[project]);
+
+        }
+
+
+    }
+
+    
+}
