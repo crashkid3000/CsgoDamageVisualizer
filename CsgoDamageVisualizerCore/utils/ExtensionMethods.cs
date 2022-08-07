fd073f743f9abaaa60ba5a75eb9a4042b382d660
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-07 21:24:12 +0200

    Moved namespace

--- CsgoDamageVisualizerCore/utils/ExtensionMethods.cs
+++ CsgoDamageVisualizerCore/utils/ExtensionMethods.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CsgoDamageVisualizerCore.extensions
+namespace CsgoDamageVisualizerCore.utils
 {
     public static class ExtensionMethods
     {
