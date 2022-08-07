b0942a82fb9a18f60d6578b2fbfc4eb27758b1b3
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-07 22:38:08 +0200

    Added test for laoding a cfg file. Currently "red"

---
+++ CsgoDamageVisualizerTests/core/loader/CfgLoaderTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+
+using CsgoDamageVisualizerCore.loader;
+using CsgoDamageVisualizerTests.core.loader.__testdriver;
+
+namespace CsgoDamageVisualizerTests.core.loader
+{
+    [TestClass]
+    public class CfgLoaderTest
+    {
+
+        private CfgLoader? loader;
+
+        [TestInitialize]
+        public void refresh()
+        {
+            if(ICsgoDamageVisualizerConfig.ConfigInstanceType == null)
+            {
+                ICsgoDamageVisualizerConfig.ConfigInstanceType = typeof(ICsgoDamageVisualizerConfigTestImpl);
+            }
+
+            loader = new CfgLoader();
+        }
+
+        [TestMethod]
+        public void LoadCfgFileAsync_LoadsLines()
+        {
+            Task<string[]> lines = loader.LoadCfgFileAsync();
+            Console.WriteLine("Reading file");
+            while (!lines.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            Assert.IsNotNull(lines.Result);
+        }
+
+    }
+}
