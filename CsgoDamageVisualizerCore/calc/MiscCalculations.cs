a3eebe685b8ce71b23f510ae8af59ed0146c540e
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-22 22:36:32 +0200

    Added misc calculations for a weapon

---
+++ CsgoDamageVisualizerCore/calc/MiscCalculations.cs
@@ -0,0 +1,30 @@
+﻿using CsgoDamageVisualizerCore.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerCore.calc
+{
+    public class MiscCalculations
+    {
+
+        Weapon weapon;
+
+        public MiscCalculations(Weapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        /// <summary>
+        /// The maximum player speed in <i>primary</i> fire mode, relative to the speed with a knife out.
+        /// </summary>
+        public float MaxPlayerSpeedRelative { get { return weapon.MaxPlayerSpeed / 250.0f; } }
+
+        /// <summary>
+        /// The maximum player speed in <i>secondary</i> fire mode, relative to the speed with a knife out.
+        /// </summary>
+        public float MaxPlayerSpeedAltRelative { get { return weapon.MaxPlayerSpeedAlt / 250.0f; } }
+    }
+}
