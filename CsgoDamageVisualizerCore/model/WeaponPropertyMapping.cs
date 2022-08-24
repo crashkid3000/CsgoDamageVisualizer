b6684d48c40f890f372032600a718033c31044fa
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-24 21:57:28 +0200

    Added special Name that inicates that a field should not be considered for coyping over

--- CsgoDamageVisualizerCore/model/WeaponPropertyMapping.cs
+++ CsgoDamageVisualizerCore/model/WeaponPropertyMapping.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace CsgoDamageVisualizerCore.model
 {
+    /// <summary>
+    /// Property that names a <c>CfgWeapon</c> field whose value will be copied over to a <c>Weapon</c> instance during object crteation.
+    /// </summary>
     internal class WeaponPropertyMapping : Attribute
     {
         public string CfgWeaponFieldName { get; init; }
+
+        /// <summary>
+        /// Use this to specify that explicitly, no conversion from CfgWeapon to Weapon is is intended
+        /// </summary>
+        public static string NO_CONVERSION = "no conversion";
 
         public WeaponPropertyMapping(string name)
         {
