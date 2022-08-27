ef1ea167f79f6744dcc367203eb761c38e805557
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-27 18:01:41 +0200

    Added economy analysis

---
+++ CsgoDamageVisualizerCore/analysis/EconomyAnalysis.cs
@@ -0,0 +1,45 @@
+﻿using CsgoDamageVisualizerCore.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerCore.analysis
+{
+    /// <summary>
+    /// This class is about analyzing the economical impact of the weapon
+    /// </summary>
+    public class EconomyAnalysis
+    {
+        private Weapon weapon { get; set; }
+
+        public EconomyAnalysis(Weapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        #region constants
+
+        /// <summary>
+        /// The default kill award that is earned upon killing an enemy when no other number is specified.
+        /// </summary>
+        public static int DefaultKillAward { get { return 300; } }
+
+        #endregion
+
+        #region analysis
+
+        /// <summary>
+        /// <para>A multiplier, telling how much more (or less) the weapon earns per kill</para><para><u>Unit:</u> 1 </para>
+        /// </summary>
+        public float KillAwardRelative { get { return weapon.KillAward / (float)DefaultKillAward; } }
+
+        /// <summary>
+        /// <para>How many kills one has to achieve until the gun has paid for itself in kill reward money</para><para><u>Unit:</u> 1 (kills)</para>
+        /// </summary>
+        public int KillsUntilAmortized { get { return (int)Math.Ceiling((double)weapon.InGamePrice / weapon.KillAward); } }
+
+        #endregion
+    }
+}
---
+++ CsgoDamageVisualizerTests/core/analysis/EconomyAnalysisTest.cs
@@ -0,0 +1,46 @@
+﻿using CsgoDamageVisualizerCore.analysis;
+using CsgoDamageVisualizerCore.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerTests.core.analysis
+{
+    [TestClass]
+    public class EconomyAnalysisTest
+    {
+        private Weapon weapon = new Weapon();
+        private EconomyAnalysis? economyAnalysis;
+
+        private Weapon createWeapon()
+        {
+            Weapon retVal = new Weapon();
+            typeof(Weapon).GetProperty(nameof(Weapon.KillAward)).SetValue(retVal, 400);
+            typeof(Weapon).GetProperty(nameof(Weapon.InGamePrice)).SetValue(retVal, 3750);
+            return retVal;
+        }
+
+        [TestInitialize]
+        public void setUp()
+        {
+            this.weapon = createWeapon();
+            this.economyAnalysis = new EconomyAnalysis(weapon);
+        }
+
+        [TestMethod]
+        public void KillAwardRelative_Works()
+        {
+            float expected = weapon.KillAward / (float)EconomyAnalysis.DefaultKillAward;
+            Assert.AreEqual(expected, economyAnalysis.KillAwardRelative);
+        }
+
+        [TestMethod]
+        public void KillsUntilAmortization_Works()
+        {
+            int expected = (int)Math.Ceiling(weapon.InGamePrice / (double)weapon.KillAward);
+            Assert.AreEqual(expected, economyAnalysis.KillsUntilAmortized);
+        }
+    }
+}
