ceda457af9f7e9d8fad21f9852ab1d1e6de393ae
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-27 17:17:52 +0200

    Refactoring, added docs, added tests

---
+++ CsgoDamageVisualizerCore/analysis/MiscAnalysis.cs
@@ -0,0 +1,47 @@
+﻿using CsgoDamageVisualizerCore.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerCore.analysis
+{
+    /// <summary>
+    /// Class that deals with analyzing a weapon, with no clear topic in mind. In other words, this class holds the calculations that don't fit anywhere else.
+    /// </summary>
+    public class MiscAnalysis
+    {
+
+        Weapon weapon;
+
+        public MiscAnalysis(Weapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        #region constants
+
+        /// <summary>
+        /// The speed of the player in units per second with a knife equipped.
+        /// </summary>
+        public static float KnifeSpeed { get { return 250.0f; } } //units per second
+
+        #endregion
+
+        #region analysis
+
+        /// <summary>
+        /// The maximum player speed in <i>primary</i> fire mode, relative to the speed with a knife out.
+        /// </summary>
+        public float MaxPlayerSpeedRelative { get { return weapon.MaxPlayerSpeed / KnifeSpeed; } }
+
+        /// <summary>
+        /// The maximum player speed in <i>secondary</i> fire mode, relative to the speed with a knife out.
+        /// </summary>
+        public float MaxPlayerSpeedAltRelative { get { return weapon.MaxPlayerSpeedAlt / KnifeSpeed; } }
+
+        #endregion
+    }
+}
---
+++ CsgoDamageVisualizerTests/core/analysis/MiscAnalysisTest.cs
@@ -0,0 +1,48 @@
+﻿using CsgoDamageVisualizerCore.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsgoDamageVisualizerCore.analysis;
+
+namespace CsgoDamageVisualizerTests.core.analysis
+{
+
+    [TestClass]
+    public class MiscAnalysisTest
+    {
+        private Weapon weapon = new Weapon();
+        private MiscAnalysis? analysis;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            this.weapon = createWeapon();
+            this.analysis = new MiscAnalysis(weapon);
+        }
+    
+    
+        private Weapon createWeapon()
+        {
+            Weapon retVal = new Weapon();
+            typeof(Weapon).GetProperty(nameof(Weapon.MaxPlayerSpeed)).SetValue(retVal, 240);
+            typeof(Weapon).GetProperty(nameof(Weapon.MaxPlayerSpeedAlt)).SetValue(retVal, 125);
+            return retVal;
+        }
+
+        [TestMethod]
+        public void MaxPlayerSpeedRelative_Works() {
+            float expectedResult = weapon.MaxPlayerSpeed / 250.0f;
+            Assert.AreEqual(expectedResult, analysis.MaxPlayerSpeedRelative);
+        }
+
+        [TestMethod]
+        public void MaxPlayerSpeedAltRelative_Works()
+        {
+            float expectedResult = weapon.MaxPlayerSpeedAlt / 250.0f;
+            Assert.AreEqual(expectedResult, analysis.MaxPlayerSpeedAltRelative);
+        }
+    }
+}
