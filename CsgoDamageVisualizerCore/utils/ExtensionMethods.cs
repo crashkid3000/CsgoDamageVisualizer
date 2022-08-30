c8b6ec6f5c8d67d1295b1440e40a8398a746b894
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-30 22:00:15 +0200

    Added extension method to get a primary or a secondary value.

--- CsgoDamageVisualizerCore/utils/ExtensionMethods.cs
+++ CsgoDamageVisualizerCore/utils/ExtensionMethods.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CsgoDamageVisualizerCore.model;
+
 namespace CsgoDamageVisualizerCore.utils
 {
     public static class ExtensionMethods
@@ -25,5 +27,23 @@ namespace CsgoDamageVisualizerCore.utils
                 return c.Equals(' ') && !firstAlphaCharSeen;
             });
         }
+
+#pragma warning disable IDE0060
+// While no instance of Weapon is needed for any method, I'd still like to keep them this closely related ebcause we access a field directly from Weapon
+        public static float ValueOrBackupValue(this Weapon w, float primaryValue, float backupValue)
+        {
+            return primaryValue.Equals(Weapon.NOT_FILLED_FLOAT) ? backupValue : primaryValue;
+        }
+
+        public static int ValueOrBackupValue(this Weapon w, int primaryValue, int backupValue)
+        {
+            return primaryValue.Equals(Weapon.NOT_FILLED_INT) ? backupValue : primaryValue;
+        }
+
+        public static string ValueOrBackupValue(this Weapon w, string primaryValue, string backupValue)
+        {
+            return primaryValue.Equals("") ? backupValue : primaryValue;
+        }
+#pragma warning restore
     }
 }
