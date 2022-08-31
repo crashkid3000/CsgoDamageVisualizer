3e80890eb746eff6c08c27d7aa17ccb11b63cb13
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-08-31 22:16:11 +0200

    Added more inaccuracy analysis. Todo: tests

--- CsgoDamageVisualizerCore/analysis/InaccuracyAnalysis.cs
+++ CsgoDamageVisualizerCore/analysis/InaccuracyAnalysis.cs
@@ -32,6 +32,24 @@ namespace CsgoDamageVisualizerCore.analysis
         public float LadderInaccuracy { get { return weapon.Spread + weapon.InaccuracyLadder; } }
         public float LadderInaccuracyAlt { get { return weapon.ValueOrBackupValue(weapon.SpreadAlt, weapon.Spread) + weapon.ValueOrBackupValue(weapon.InaccuracyLadderAlt, weapon.InaccuracyLadder); } }
 
+        public float MovementInaccuracy { get { return StandingInaccuracy + weapon.InaccuracyMove; } }
+        public float MovementInaccuracyAlt { get { return StandingInaccuracyAlt + weapon.ValueOrBackupValue(weapon.InaccuracyMoveAlt, weapon.InaccuracyMove); } }
+
+        public float JumpingInitialInaccuracy { get { return weapon.Spread + weapon.InaccuracyJump + weapon.ValueOrBackupValue(weapon.InaccuracyJumpInitial, 0.0f) ; } }
+        public float JumpingInitialInaccuracyAlt { get { return weapon.ValueOrBackupValue(weapon.SpreadAlt, weapon.Spread)
+                    + weapon.ValueOrBackupValue(weapon.InaccuracyJumpAlt, weapon.InaccuracyJump)
+                    + weapon.ValueOrBackupValue(weapon.ValueOrBackupValue(weapon.InaccuracyJumpInitialAlt, 0.0f), weapon.ValueOrBackupValue(weapon.InaccuracyJumpInitial, 0.0f)); } }
+
+        public float JumpingApexInaccuracy { get { return weapon.Spread + weapon.InaccuracyJump + weapon.InaccuracyJumpApex; } }
+        public float JumpingApexInaccuracyAlt
+        {
+            get
+            {
+                return weapon.ValueOrBackupValue(weapon.SpreadAlt, weapon.Spread)
+                    + weapon.ValueOrBackupValue(weapon.InaccuracyJumpAlt, weapon.InaccuracyJump)
+                    + weapon.ValueOrBackupValue(weapon.InaccuracyJumpApexAlt, weapon.InaccuracyJumpApex);
+            }
+        }
 
     }
 }
