6324ad10d9f27143f1b14c90e2d9b7aee10d4ed5
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-09-08 23:01:40 +0200

    Added attempt at firing inaccuracy calculation with transitional recovery time (i.e. it changes after firing x bullets)

---
+++ CsgoDamageVisualizerCore/analysis/TransitionableRecoveryTimeInaccuracyCalculation.cs
@@ -0,0 +1,76 @@
+﻿using CsgoDamageVisualizerCore.model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsgoDamageVisualizerCore.analysis
+{
+    public class TransitionableRecoveryTimeInaccuracyCalculation: IRecoveryTimeInaccuracyCalculation
+    {
+        private InaccuracyAnalysis Analysis { get; init; }
+        private Weapon CurrentWeapon { get; init; }
+        /// <summary>
+        /// Whether the user is currently standing or crouching
+        /// </summary>
+        private bool IsStanding { get; init; }
+
+        public TransitionableRecoveryTimeInaccuracyCalculation(Weapon w, bool isStanding = true)
+        {
+            this.Analysis = new InaccuracyAnalysis(w);
+            this.CurrentWeapon = w;
+            this.IsStanding = isStanding;
+        }
+
+        public float Calculate(float userCycleTime, int shotsFired)
+        {
+            /* 
+             * Use similar algorithm to Classic, but only do the following: smoothly swap recovery times. Leave everythin as is, and then try again
+             */
+            return float.NaN;
+        }
+
+        //public float Calculate(float userCycleTime, int shotsFired)
+        //{
+        //    float retVal = 0.0f;
+        //    float recoveryTimeStart = IsStanding ? CurrentWeapon.RecoveryTimeStand : CurrentWeapon.RecoveryTimeCrouch;
+        //    float recoveryTimeFinal = IsStanding ? CurrentWeapon.RecoveryTimeStandFinal : CurrentWeapon.RecoveryTimeCrouchFinal;
+        //    InaccuracyCalculations calculations = new InaccuracyCalculations();
+        //    for(int i = 0; i < shotsFired; i++)
+        //    {
+        //        float shareOfFinalRecoveryTime = 1 - ((Math.Max(i, Math.Max(CurrentWeapon.RecoveryTransitionStartBullet, 0)) - CurrentWeapon.RecoveryTransitionEndBullet) / (float)Analysis.InaccuracyTransitionPeriod);
+        //        float shareOfInitialRecoveryTime = 1 - shareOfFinalRecoveryTime;
+        //        float averagedRecoveryTime = (shareOfInitialRecoveryTime * recoveryTimeStart) + (shareOfFinalRecoveryTime * recoveryTimeFinal);
+        //        float sprayFactor = GetBulletSprayFactor(userCycleTime, i + 1, averagedRecoveryTime);
+        //        float inaccuracyOfShot = calculations.CalculateExtraInaccuracyAfterTime(CurrentWeapon.InaccuracyFire, averagedRecoveryTime, sprayFactor * userCycleTime);
+        //        retVal += inaccuracyOfShot;
+        //        Debug.WriteLine($"  Added inaccuracy: {inaccuracyOfShot}");
+        //    }
+
+        //    return retVal;
+        //}
+
+        ///// <summary>
+        ///// Estimates how much spraying actually took place, considering the bullets fired and how quickly they were fired apart from each other. Assumption: shots fired with always the same delay after each other
+        ///// </summary>
+        ///// <param name="userCycletime"></param>
+        ///// <returns></returns>
+        //private float GetBulletSprayFactor(float userCycletime, int shotsFired, float recoveryTime)
+        //{
+        //    float retVal = 0.0f;
+        //    for(int i = 0; i < shotsFired; i++)
+        //    {
+        //        float sprayFactor = Math.Min(CurrentWeapon.Cycletime / userCycletime, 1);
+        //        float decayedSprayFactor = (float)Math.Pow(InaccuracyCalculations.InaccuracyDecayExponent, sprayFactor / recoveryTime);
+        //        retVal += decayedSprayFactor;
+        //    }
+        //    return shotsFired * CurrentWeapon.Cycletime / userCycletime; //todo: Use Cycletime, RecoveryTime, a value inbetween?
+        //}
+
+
+
+
+    }
+}
---
+++ CsgoDamageVisualizerTests/core/analysis/TransitionableRecoveryTimeInaccuracyCalculationTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsgoDamageVisualizerCore.analysis;
+using CsgoDamageVisualizerCore.model;
+
+namespace CsgoDamageVisualizerTests.core.analysis
+{
+
+    [TestClass]
+    public class TransitionableRecoveryTimeInaccuracyCalculationTest
+    {
+
+        private TransitionableRecoveryTimeInaccuracyCalculation transitional;
+        private ClassicRecoveryTimeInaccuracyCalculation classic;
+
+        public TransitionableRecoveryTimeInaccuracyCalculationTest()
+        {
+            CfgWeapon cfgFiveSeven = new CfgWeapon();
+            CfgWeapon.SetValue(cfgFiveSeven, "__name", "fiveseven_prefab");
+            CfgWeapon.SetValue(cfgFiveSeven, "cycletime", "0.15");
+            CfgWeapon.SetValue(cfgFiveSeven, "spread", "2.0");
+            CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyCrouch", "6.83");
+            CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyStand", "9.1");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyJumpInitial", "78.79");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyJump", "109.0");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyLand", "0.188");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyLadder", "83.66");
+            CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyFire", "25.0");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyMove", "136.01");
+            CfgWeapon.SetValue(cfgFiveSeven, "recoveryTimeCrouch", "0.2");
+            CfgWeapon.SetValue(cfgFiveSeven, "recoveryTimeStand", "0.2");
+            //CfgWeapon.SetValue(cfgFiveSeven, "spreadAlt", "0.30");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyCrouchAlt", "3.05");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyStandAlt", "3.81");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyJumpAlt", "109.000");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyLandAlt", "0.188");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyLadderAlt", "138.758");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyFireAlt", "9.20");
+            //CfgWeapon.SetValue(cfgFiveSeven, "inaccuracyMoveAlt", "136.01");
+            CfgWeapon.SetValue(cfgFiveSeven, "recoveryTimeCrouchFinal", "0.5");
+            CfgWeapon.SetValue(cfgFiveSeven, "recoveryTimeStandFinal", "0.5");
+            CfgWeapon.SetValue(cfgFiveSeven, "recoveryTransitionStartBullet", "0");
+            CfgWeapon.SetValue(cfgFiveSeven, "recoveryTransitionEndBullet", "5");
+
+            Weapon w = new Weapon(cfgFiveSeven);
+
+            transitional = new TransitionableRecoveryTimeInaccuracyCalculation(w);
+            classic = new ClassicRecoveryTimeInaccuracyCalculation(w);
+        }
+
+        [TestMethod]
+        public void a()
+        {
+            int bulletsFired = 15;
+
+            float fireRate = 0.15f;
+            float standingInaccuracy = 11.0f;
+            float inaccuracy = transitional.Calculate(fireRate, bulletsFired);
+            Console.WriteLine($"After Bullet {bulletsFired} - Extra Inaccuracy {inaccuracy} - Total Inaccuracy {inaccuracy + standingInaccuracy}");
+            
+        }
+
+        [TestMethod]
+        public void classicInaccuracy()
+        {
+            int bulletsFired = 15;
+            float fireRate = 0.15f;
+            float standingInaccuracy = 11.0f;
+            float inaccuracy = classic.Calculate(fireRate, bulletsFired);
+            Console.WriteLine($"After Bullet {bulletsFired} - Extra Inaccuracy {inaccuracy} - Total Inaccuracy {inaccuracy + standingInaccuracy}");
+        }
+
+
+    }
+}
